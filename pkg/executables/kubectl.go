e9d604a6cb74251b3b9ed46fe57d12df64ea7499
Abdullahi Abdinur <acool@amazon.com>
2022-03-28 07:29:40 -0700

    Complete install implementation

--- pkg/executables/kubectl.go
+++ pkg/executables/kubectl.go
@@ -1546,8 +1546,8 @@ func (k *Kubectl) DescribePackages(ctx context.Context, opts ...KubectlOpt) (byt
 	return stdOut, err
 }
 
-func (k *Kubectl) ApplyResourcesFromBytes(ctx context.Context, data []byte) error {
-	params := []string{"apply", "-f", "-"}
+func (k *Kubectl) ApplyResourcesFromBytes(ctx context.Context, data []byte, kubeConfig string) error {
+	params := []string{"apply", "-f", "-", "--kubeconfig", kubeConfig}
 	_, err := k.ExecuteWithStdin(ctx, data, params...)
 	if err != nil {
 		return fmt.Errorf("executing apply: %v", err)
