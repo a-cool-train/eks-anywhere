3e60fcf1bc60632d7fa69993d2b38ad9a59251e5
Abdullahi Abdinur <acool@amazon.com>
2022-04-12 06:47:42 -0700

    Check for empty packagesLocation

--- pkg/workflows/create.go
+++ pkg/workflows/create.go
@@ -63,8 +63,10 @@ func (c *Create) Run(ctx context.Context, clusterSpec *cluster.Spec, validator i
 		return err
 	}
 
-	installCuratedPackages(ctx, clusterSpec.Cluster.Name, packagesLocation)
-	return err
+	if packagesLocation != "" {
+		installCuratedPackages(ctx, clusterSpec.Cluster.Name, packagesLocation)
+	}
+	return nil
 }
 
 // task related entities
